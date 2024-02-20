6509dc43a0509578ec1e5e1095f118085862db0c
RoigChristian1 <c.roig90@gmail.com>
2024-02-20 17:04:10 +0100

    fix typo

--- cypress/e2e/exercise2.cy.js
+++ cypress/e2e/exercise2.cy.js
@@ -1,29 +1,28 @@
 describe('Exercise 1', () => {
-    const baseUrl = 'http://api-url';
-  
-    const authToken = () => {
-        //Implement a function to obtain the proper token for the methods
-        return token
-    };
-  
-    it('Verifying HTTP status 403 for countryCode RU on all endpoints', () => {
-      const authorizedEndpoints = [
-        '/rest/Endpoint1',
-        '/rest/Endpoint2',
-        // ...
-        '/rest/Endpoint150',
-      ];
-        authorizedEndpoints.forEach((endpoint) => {
-          cy.request({
-            method: 'GET',
-            url: `${baseUrl}${endpoint}?countryCode=RU&languageCode=en&searchText=example`,
-            headers: {
-              Authorization: `Bearer ${authToken}`,
-            },
-            failOnStatusCode: false,
-          }).then((response) => {
-            cy.wrap(response.status).should('eq', 403);
-          });
+  const baseUrl = 'http://api-url';
+
+  const authToken = () => {
+    //Implement a function to obtain the proper token for the methods
+  };
+
+  it('Verifying HTTP status 403 for countryCode RU on all endpoints', () => {
+    const authorizedEndpoints = [
+      '/rest/Endpoint1',
+      '/rest/Endpoint2',
+      // ...
+      '/rest/Endpoint150',
+    ];
+    authorizedEndpoints.forEach((endpoint) => {
+      cy.request({
+        method: 'GET',
+        url: `${baseUrl}${endpoint}?countryCode=RU&languageCode=en&searchText=example`,
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(403)
       });
     });
+  });
 })
