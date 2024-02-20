fa5383b7e196097e58b671e3e9978a08957dde82
RoigChristian1 <c.roig90@gmail.com>
2024-02-20 17:32:02 +0100

    Adding test to other country

--- cypress/e2e/exercise2.cy.js
+++ cypress/e2e/exercise2.cy.js
@@ -5,13 +5,14 @@ describe('Exercise 1', () => {
     //Implement a function to obtain the proper token for the methods
   };
 
+  const authorizedEndpoints = [
+    '/rest/Endpoint1',
+    '/rest/Endpoint2',
+    // ...
+    '/rest/Endpoint150',
+  ];
+
   it('Verifying HTTP status 403 for countryCode RU on all endpoints', () => {
-    const authorizedEndpoints = [
-      '/rest/Endpoint1',
-      '/rest/Endpoint2',
-      // ...
-      '/rest/Endpoint150',
-    ];
     authorizedEndpoints.forEach((endpoint) => {
       cy.request({
         method: 'GET',
@@ -24,5 +25,20 @@ describe('Exercise 1', () => {
         expect(response.status).to.eq(403)
       });
     });
+  })
+
+    it('Verifying HTTP status 200 for countryCode EX on all endpoints', () => {
+      authorizedEndpoints.forEach((endpoint) => {
+        cy.request({
+          method: 'GET',
+          url: `${baseUrl}${endpoint}?countryCode=EX&languageCode=en&searchText=example`,
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+          failOnStatusCode: false,
+        }).then((response) => {
+          expect(response.status).to.eq(200)
+        });
+      });
   });
 })
