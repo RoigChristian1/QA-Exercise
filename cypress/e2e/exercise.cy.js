98210162ea9faddb40ee25a624ce263336499b98
RoigChristian1 <c.roig90@gmail.com>
2024-02-21 17:48:06 +0100

    Refactor

--- cypress/e2e/exercise.cy.js
+++ cypress/e2e/exercise.cy.js
@@ -1,42 +1,28 @@
+import {apiRequest} from '../support/commands'
+import {authorizedEndpoints} from '../support/e2e.js'
+
 describe('Exercise 1', () => {
   const baseUrl = 'http://api-url';
+  beforeEach((() => {
+     const getAuthToken = () => {
+      //Implement a function to obtain the proper token for the methods
+    };
+  }))
 
-  const authToken = () => {
-    //Implement a function to obtain the proper token for the methods
-  };
-
-  const authorizedEndpoints = [
-    '/rest/Endpoint1',
-    '/rest/Endpoint2',
-    // ...
-    '/rest/Endpoint150',
-  ];
 
   it('Verifying HTTP status 403 for countryCode RU on all endpoints', () => {
+    const authToken = getAuthToken();
     authorizedEndpoints.forEach((endpoint) => {
-      cy.request({
-        method: 'GET',
-        url: `${baseUrl}${endpoint}?countryCode=RU&languageCode=en&searchText=example`,
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-        failOnStatusCode: false,
-      }).then((response) => {
+      apiRequest(baseUrl,endpoint,'RU',authToken).then((response) => {
         expect(response.status).to.eq(403)
       });
     });
   })
 
     it('Verifying HTTP status 200 for countryCode EX on all endpoints', () => {
+      const authToken = getAuthToken();
       authorizedEndpoints.forEach((endpoint) => {
-        cy.request({
-          method: 'GET',
-          url: `${baseUrl}${endpoint}?countryCode=EX&languageCode=en&searchText=example`,
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-          },
-          failOnStatusCode: false,
-        }).then((response) => {
+        apiRequest(baseUrl,endpoint,'EX',authToken).then((response) => {
           expect(response.status).to.eq(200)
         });
       });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,13 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+export {apiRequest}
+
+const apiRequest = (baseUrl,endpoint,countryCode,authToken) => {
+    cy.request({
+        method: 'GET',
+        url: `${baseUrl}${endpoint}?countryCode=${countryCode}&languageCode=en&searchText=example`,
+        auth: { username, password },
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+        failOnStatusCode: false,
+      })
+}
